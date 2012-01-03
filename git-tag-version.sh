cbcb1df1cf8aeea9548df2b6392dcc9cf2cd50a2
Mike McClurg <mike.mcclurg@citrix.com>
2012-01-03 16:53:31 +0000

    git-tag-version.sh now bumps Debian changelog, if it exists
    Signed-off-by: Mike McClurg <mike.mcclurg@citrix.com>

--- git-tag-version.sh
+++ git-tag-version.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Tag the master and debian branch HEADs with the version in ./VERSION,
-# according to format specified in ./debian/gbp.conf
+# Tag the master branch HEAD with the version in ./VERSION, according to format
+# specified in ./debian/gbp.conf
 
 VERSION=$(cat VERSION | tr -d '\n')
 
-# Retag master and debian
+# Retag master
 git tag -f -a master/${VERSION} master -m "Tagged master branch with version $version"
-#git tag -f -a debian/${VERSION} debian -m "Tagged debian branch with version $version"
+
+# Bump debian changelog
+git checkout -q debian || true
+[ -f debian/changelog ] || exit 1
+dch -v "${VERSION}-1" "Upstream version bump" --distribution "unstable" --force-distribution
+git add debian/changelog
+git commit -s -m "Bump changelog to upstream version: ${VERSION}-1"
